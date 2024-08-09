b0c138fdaf50177c1cd702d33c5d99568882972b
Chirayu Maru <chirayumaru@gmail.com>
2024-08-09 18:47:15 +0530

    feature

--- src/components/Lights.jsx
+++ src/components/Lights.jsx
@@ -2,7 +2,7 @@ import { Environment, Lightformer } from "@react-three/drei";
 
 const Lights = () => {
   return (
-    // group different lights and lightformers. We can use group to organize lights, cameras, meshes, and other objects in the scene.
+    
     <group name="lights">
       {/**
        * @description Environment is used to create a background environment for the scene
